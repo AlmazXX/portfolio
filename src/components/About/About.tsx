9d7ecde36d5e836e00d3079dbeae3d3ef56b7ca3
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-29 14:47:15 +0600

    Created About comp, added styles

---
+++ src/components/About/About.tsx
@@ -0,0 +1,28 @@
+import './About.css';
+import about from '../../assets/images/about-img.png';
+
+const About = () => {
+  return (
+    <section className="section about" id="about">
+      <div className="about__container container grid">
+        <div className="about__data">
+          <h2 className="section__title about__title">
+            About Halloween <br /> Night
+          </h2>
+          <p className="about__description">
+            Night of all the saints, or all the dead, is celebrated on October
+            31 and it is a very fun international celebration, this celebration
+            comes from ancient origins, and is already celebrated by everyone.
+          </p>
+          <a href="#" className="button">
+            Know more
+          </a>
+        </div>
+
+        <img src={about} alt="" className="about__img" />
+      </div>
+    </section>
+  );
+};
+
+export default About;
